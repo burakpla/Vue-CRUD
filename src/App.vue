3c4362354e5ba3e36c40f699e9bb5b6d5f32b8d5
Burak Pala <burak.pala1912@gmail.com>
2021-11-10 21:56:09 +0300

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
   <div id="app">
-    <ProductList/>
+    <ProductList @delete:product="deleteProduct" @update:product="updateProduct" :products="products"/>
+            <ProductAdd @add:product="addProduct"/>
+
   </div>
 </template>
 
 <script>
 import ProductList from './components/ProductList.vue'
+import ProductAdd from './components/ProductAdd.vue'
 
 export default {
   name: 'App',
   components: {
-    ProductList
+    ProductList,
+    ProductAdd
+  },
+  data(){
+    return{
+      products:[
+        {id:1,productName:'Laptop',categoryId:'MacbookAir',quantityPerUnit:'New Generation',unitPrice:'1000$',unitsInStock:10},
+        {id:2,productName:'Parfume',categoryId:'Dior Sauvage',quantityPerUnit:'Best Parfume Ever',unitPrice:'60$',unitsInStock:8},
+        {id:3,productName:'Food',categoryId:'Cake',quantityPerUnit:'Mini Cake',unitPrice:'1.5$',unitsInStock:55}
+      ]
+    }
+  },
+  methods:{
+    deleteProduct(product){
+      this.products=this.products.filter(
+        productToFilter=>productToFilter.id!==product.id
+      )
+    },
+    updateProduct(){
+
+    },
+    addProduct(product){
+      const newProduct={...product}
+      this.products=[...this.products,newProduct]
+    }
   }
 }
 </script>
@@ -20,7 +47,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
